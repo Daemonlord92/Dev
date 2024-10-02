5b007be0c53c15fb613a7a1eddb642479cdff964
matthewjeshuamartin <matthew-martin@programmer.net>
2024-10-02 12:23:40 -0400

    TypeScript Crash Course Complete

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,2 +1,48 @@
 var message = "Hello, World";
 console.log(message);
+var person = {
+    firstName: "Nazi",
+    //lastName: "Doh",
+    height: 5,
+    //weight: 155,
+    age: 2,
+    run: function () {
+    },
+    eat: function () {
+        throw new Error("Function not implemented.");
+    }
+};
+var Animal = /** @class */ (function () {
+    function Animal(name, age, dob) {
+        this.name = name;
+        this.age = age;
+        this.dob = dob;
+    }
+    Animal.prototype.getName = function () {
+        return this.name;
+    };
+    Animal.prototype.setName = function (name) {
+        this.name = name;
+    };
+    Animal.prototype.getAge = function () {
+        return this.age;
+    };
+    Animal.prototype.setAge = function () {
+        this.age = (new Date().getFullYear() - this.dob.getFullYear());
+    };
+    Animal.prototype.getDob = function () {
+        return this.dob.getDate();
+    };
+    Animal.prototype.setDob = function (dob) {
+        this.dob = dob;
+    };
+    return Animal;
+}());
+var hamster = new Animal("Tali", 5, new Date("03-21-2021"));
+console.table(hamster);
+hamster.setAge();
+console.table(hamster);
+function identity(person) {
+    return person.firstName;
+}
+console.log(identity(person));
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,3 +1,83 @@
 let message: string = "Hello, World"
 
 console.log(message)
+
+interface Person {
+    firstName: string
+    lastName?: string
+    height: number
+    weight?: number
+    age: number
+    hair?: string
+    run?: () => void
+    eat: () => string 
+}
+
+const person: Person = {
+    firstName: "Nazi",
+    //lastName: "Doh",
+    height: 5,
+    //weight: 155,
+    age: 2,
+    run: function (): void {
+        
+    },
+    eat: function (): string {
+        throw new Error("Function not implemented.")
+    }
+}
+
+class Animal {
+    //Field Variable
+    private name: string
+    private age: number
+    private dob: Date
+
+    constructor(name: string, age:number, dob:Date){
+        this.name = name
+        this.age = age
+        this.dob = dob
+    }
+
+    public getName(): string {
+        return this.name
+    }
+
+    public setName(name: string): void {
+        this.name = name
+    }
+
+    public getAge(): number {
+        return this.age
+    }
+
+    public setAge(): void {
+        this.age = (new Date().getFullYear() - this.dob.getFullYear())
+    }
+
+    public getDob(): number {
+        return this.dob.getDate()
+    }
+
+    public setDob(dob:Date): void {
+        this.dob = dob
+    }
+}
+
+let hamster = new Animal("Tali", 5, new Date("03-21-2021"))
+
+console.table(hamster)
+
+hamster.setAge()
+
+console.table(hamster)
+
+
+function identity<T>(person: T):T {
+    return person
+}
+
+
+console.log(identity<Person>(person))
+
+identity<Animal>(hamster)
