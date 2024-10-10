d73a644ac9151c9f68234733704ac88a7205121f
matthewjeshuamartin <matthew-martin@programmer.net>
2024-10-10 14:08:13 -0400

    Delete Path Set

---
+++ monster-trainer-fe/src/component/monster/DeleteMonster.tsx
@@ -0,0 +1,54 @@
+import { Box, Button, ButtonGroup, Modal, Typography } from "@mui/material";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+
+function DeleteMonster({id, open, closeModal, openModal}:{id:number, open:boolean, closeModal:()=>void, openModal:()=>void}) {
+
+    const queryClient = useQueryClient();
+
+    const deleteMonster = async () => {
+        const request = await fetch(import.meta.env.VITE_API_URL + "/monster/delete/" + id, {
+            method: 'DELETE'
+        })
+        const response = request.json()
+        return response;
+    }
+
+    const mutation = useMutation({
+        mutationFn: deleteMonster,
+        onSuccess:() => {
+            alert("Monster has been deleted")
+            queryClient.invalidateQueries()
+            closeModal()
+        }
+    })
+
+    return (
+        <>
+            <Button onClick={openModal}>
+                Delete Monster
+            </Button>
+            <Modal
+                open={open}
+                onClose={closeModal}
+                >
+                   <Box color={"white"} sx={{my:10, mx:'auto', maxWidth:50, maxHeight: 50}}>
+                        <Typography variant="h4">
+                            Do you want to delete this Monster
+                        </Typography>
+                        <br />
+                        <ButtonGroup variant="contained">
+                            <Button onClick={() => mutation.mutate()}>
+                                Yes
+                            </Button>
+                            <Button onClick={() => closeModal()}>
+                                No
+                            </Button>
+                        </ButtonGroup>
+                    </Box> 
+                </Modal>
+        </>
+    );
+}
+
+export default DeleteMonster;
--- monster-trainer-fe/src/component/monster/MonsterRow.tsx
+++ monster-trainer-fe/src/component/monster/MonsterRow.tsx
@@ -1,10 +1,18 @@
 import { TableCell, TableRow } from "@mui/material";
 import Monster from "../../types/Monster";
 import { useNavigate } from "react-router-dom";
+import DeleteMonster from "./DeleteMonster";
+import { useState } from "react";
 
 function MonsterRow({data}:{data:Monster}) {
     const navigate = useNavigate()
     const {id, name, species, attackPower, defensePower, price} = data
+
+    const [open, setOpen] = useState(false)
+
+    const openModal = () => setOpen(true)
+    const closeModal = () => setOpen(false)
+
     return (
         <TableRow key={id} hover tabIndex={-1}>
             <TableCell>
@@ -24,6 +32,9 @@ function MonsterRow({data}:{data:Monster}) {
             </TableCell>
             <TableCell>
                 <a onClick={() => navigate("/update", {state: data})}>Update Monster</a>
+            </TableCell>
+            <TableCell>
+                <DeleteMonster id={id} open={open} openModal={openModal} closeModal={closeModal} />
             </TableCell>
         </TableRow>
     );
